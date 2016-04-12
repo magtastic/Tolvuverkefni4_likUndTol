74c44ebe412a4716f2a6c1adccfbd82d549f0991
magtastic <magnusol93@gmail.com>
2016-04-12 13:22:17 +0000

    Tilbúið!
    Allt bara komið!

--- tolvuverkefni4.R
+++ tolvuverkefni4.R
@@ -1,65 +1,178 @@
-medaltal = 96
+#global breytur
+medalgildi = 96
 stadalfravik = 13
 gildiN = c(5,9,16,28,50,90,160,280,500,
            900,1600,2800,5000,9000,16000)
 
-#Skilar melit... held eg...o-inu = s^1
-#ur vendilbreytum af n breytistaerdum
-#med videigandi medaltali og stadalfraviki.
-MetillUrtaks = function(n){
-  itrun = rnorm(n,mean = medaltal, sd = stadalfravik)
-  medaltalItrun = mean(itrun)
+#skilar n fjolda af vendigildum thar sem medalgildi er 96
+#og stadalfravik er 13
+vendiGildi = function(n){
+  return(rnorm(n,mean = medalgildi, sd = stadalfravik))
+}
+
+reiknaMetilUrGildurm = function(gildi){
+  medalgildiItrun = mean(gildi)
+  result = 0
+
+  for(i in 1:length(gildi)){
+    result = result + ((gildi[i]-medalgildiItrun)^2)
+  }
+
+  return (sqrt(result/(length(gildi)-1)))
+
+}
+
+#skilar metil1
+metill1 = function(n){
+  itrun = vendiGildi(n)
+  medalgildiItrun = mean(itrun)
   result = 0
 
   for(i in 1:n){
-    result = result + ((itrun[i]-medaltalItrun)^2)
+    result = result + ((itrun[i]-medalgildiItrun)^2)
   }
   result = sqrt(1/(n-1) * result)
 
   return(result)
 }
 
-#fjoldiItrana er bara 10000 og dreifni aetti
-#ad vera boolean breyta, TRUE ef vid erum ad reikna dreifni
-#annars FALSE.
-metlarFyrirOllGildi = function (fjoldiItrana, dreifni){
-  allirMedalMetlar = c()
+#skilar metil2
+metill2 = function(n){
+  return( IQR( vendiGildi( n ) ) / 1.349 )
+}
+
+#skilar vigri med jafn morgum gildum i eru i gildiN.
+#hvert stak i vigrinum er med fjoldiItrana gildum af
+#metill1.
+itraAllaMetlla1 = function (fjoldiItrana, dreifni){
+  allirMetlar = c()
 
   for(i in 1:length(gildiN)){
 
     summaAllraItrana = c()
 
     for(k in 1:fjoldiItrana){
-      summaAllraItrana = c(summaAllraItrana, MetillUrtaks(gildiN[i]))
+      summaAllraItrana = c(summaAllraItrana, metill1(gildiN[i]))
     }
+
     if(dreifni){
       summaAllraItrana = var(summaAllraItrana)
     }else{
-      summaAllraItrana = mean(summaAllraItrana)
-      summaAllraItrana = summaAllraItrana - MetillUrtaks(gildiN[i])
+      summaAllraItrana = mean(summaAllraItrana) - metill1(gildiN[i])
     }
-    allirMedalMetlar = c(allirMedalMetlar,summaAllraItrana)
+
+    allirMetlar = c(allirMetlar,summaAllraItrana)
     print(i)
   }
-  return(allirMedalMetlar)
+
+  return(allirMetlar)
 }
 
-#i raun daemi 1 og 2. dreifni vaeri tha TRUE i daemi 2.
-daemi1 = function (fjoldiItrana, dreifni){
+#skilar vigri med jafn morgum gildum i eru i gildiN.
+#hvert stak i vigrinum er med fjoldiItrana gildum af
+#metill2.
+itraAllaMetlla2 = function(fjoldiItrana, dreifni){
+    allirMetlar = c()
 
-  yAs = metlarFyrirOllGildi(fjoldiItrana, dreifni)
+    for(i in 1:length(gildiN)){
+      itranirAllaMetla = c()
+
+      for(k in 1:fjoldiItrana){
+          itranirAllaMetla = c( itranirAllaMetla, metill2(gildiN[i]))
+      }
+
+      if(dreifni){
+        itranirAllaMetla = var(itranirAllaMetla)
+      }else{
+        itranirAllaMetla = mean(itranirAllaMetla) - metill2(gildiN[i])
+      }
+
+      allirMetlar = c(allirMetlar, itranirAllaMetla)
+    }
+
+    return(allirMetlar)
+}
+
+Oryggisbil = function(){
+
+  x = vendiGildi(23)
+  medaltal = mean(x)
+  s = reiknaMetilUrGildurm(x)
+  z1 = qnorm(.69)
+  z2 = qnorm(.025)
+
+  check = medalgildi + z1 * stadalfravik
+
+  midja = medaltal + z1 * s
+  bil = z2*(sqrt(s^2+(0.5*z1^2*s^2))/sqrt(23))
+
+  if(check > midja + bil && check < midja - bil){
+    return (TRUE)
+  }
+  return (FALSE)
+
+}
+
+################ daemi1 er a) lidur, daemi2 er b) lidur o.s.fr. ################
+
+daemi1 = function (fjoldiItrana){
+  yAs = itraAllaMetlla1(fjoldiItrana, FALSE)
   xAs = gildiN
 
-  if(dreifni){
-    title = 'dreifni metils'
-    pdf('mynd2.pdf')
-  }else{
-    title = 'bjagi metils'
-    pdf('mynd1.pdf')
-  }
+  title = 'bjagi metils'
+  pdf('mynd1.pdf')
+  plot(x = log(xAs), y = yAs, type ='l', main = title)
+}
 
+daemi2 = function (fjoldiItrana){
+  yAs = itraAllaMetlla1(fjoldiItrana, TRUE)
+  xAs = gildiN
+
+  title = 'dreifni metils'
+  pdf('mynd2.pdf')
   plot(x = log(xAs), y = log(yAs), type ='l', main = title)
+}
+
+daemi3 = function(fjoldiItrana){
+  yAs = itraAllaMetlla1(fjoldiItrana, TRUE) +
+        itraAllaMetlla1(fjoldiItrana, FALSE)^2
+  xAs = gildiN
+
+  pdf('mynd3.pdf')
+  plot(x = log(xAs), y = log(yAs), type = 'l')
+}
+
+daemi4 = function(fjoldiItrana){
+  yAs = itraAllaMetlla2(fjoldiItrana, FALSE)
+  xAs = gildiN
+
+  pdf('mynd4.pdf')
+  plot(x=log(xAs),y=yAs,type = 'l')
+}
+
+daemi5 = function(fjoldiItrana){
+  yAs = itraAllaMetlla2(fjoldiItrana, TRUE)
+  xAs = gildiN
+
+  pdf('mynd5.pdf')
+  plot(x=log(xAs),y=log(yAs),type='l')
+}
+
+daemi6 = function(fjoldiItrana){
+  yAs = itraAllaMetlla2(fjoldiItrana, TRUE) +
+        itraAllaMetlla2(fjoldiItrana, FALSE)^2
+  xAs = gildiN
+
+  pdf('mynd6.pdf')
+  plot(x = log(xAs), y=log(yAs),type='l')
+}
+
+daemi8 = function (fjoldiItrana){
+  fjoldiSemPassar = 0
+  for(i in 1:fjoldiItrana){
+    if(Oryggisbil()){
+      fjoldiSemPassar = fjoldiSemPassar + 1
+    }
   }
-
-
-daemi1(10000, TRUE)
+  return (fjoldiSemPassar/fjoldiItrana)
+}
